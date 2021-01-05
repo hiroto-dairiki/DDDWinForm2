4e1119c0b4f7749b8cb1deb68db63ff7c02fa505
hiroto-dairiki <dairiki.hiroto@itfllc.co.jp>
2021-01-05 15:45:25 +0900

    SQLiteへ接続

--- DDDWinForm2/WeatherLatestView.cs
+++ DDDWinForm2/WeatherLatestView.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SQLite;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ namespace DDDWinForm2
 {
     public partial class WeatherLatestView : Form
     {
+        private readonly string ConnectionString = @"Data Source = C:\Users\itf\Desktop\DDD.db;Version=3;";
         public WeatherLatestView()
         {
             InitializeComponent();
@@ -27,7 +29,9 @@ from Weather
 where AreaId = @AreaId
 order by DataDate desc
 LIMIT 1
-"
+";
+            DataTable dt = new DataTable();
+            using(var connection = new SQLiteConnection()
         }
     }
 }
